179ff6fc540e582a189fa69390d394fdb81ad551
Daniel Tolentino <danieltolentino56@gmail.com>
2021-04-03 17:49:45 -0300

    retira call necessária de script

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
+
 export default class MyDocument extends Document {
     static async getInitialProps(ctx) {
         const initialProps = await Document.getInitialProps(ctx);
@@ -12,12 +13,12 @@ export default class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                <script src="https://kit.fontawesome.com/be47bb51ae.js" crossorigin="anonymous"></script>
+                {/* <script src="https://kit.fontawesome.com/be47bb51ae.js" crossOrigin="anonymous"></script> */}
                 </Head>
                 <body>
                     <Main />
                     <NextScript />
-                    <script src="https://kit.fontawesome.com/be47bb51ae.js" crossorigin="anonymous"></script>
+                    <script src="https://kit.fontawesome.com/be47bb51ae.js" crossOrigin="anonymous"></script>
                     <style dangerouslySetInnerHTML={{__html: `.owl-carousel {display: block;}.post_slider_inner.owl-carousel > .item {
                         display: inline-block;
                         width: 25%;
