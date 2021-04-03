df3dbc9ccbf84ed2e154cedebf66b4ba989dce56
Daniel Tolentino <danieltolentino56@gmail.com>
2021-04-03 18:00:55 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,4 +2,12 @@ module.exports = {
     future: {
       webpack5: true,
     },
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, serverless, webpack }) => {
+        // Note: we provide webpack above so you should not `require` it
+        // Perform customizations to webpack config
+        config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//))
+    
+        // Important: return the modified config
+        return config
   }
+}
