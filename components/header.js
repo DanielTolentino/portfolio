70de28d638871740e49f22cf14a83e9504303ff8
DanielTolentino <danieltolentino56@gmail.com>
2021-08-12 10:39:00 -0300

    Limpa códigos comentados no index.js e global.css

--- components/header.js
+++ components/header.js
@@ -4,13 +4,6 @@ export default class AppHeader extends React.Component {
     render() {
         return (
             <header>
-                <Image
-    /*     src="https://unavatar.now.sh/github/DanielTolentino"
-        alt="Picture of the author"
-        width={500}
-        height={500} */
-      />
-	{/* 	<img src="https://unavatar.now.sh/github/DanielTolentino" /> */}
         
 	</header>
         )
