f543de74270abc35318d8a99505d492924d9ce6e
Daniel Tolentino <danieltolentino@pop-os.localdomain>
2021-11-04 15:25:04 -0300

    bump nextjs version and netlify plugin

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    target:'serverless',
     webpack5: true,
+    swcMinify: true,
     generateBuildId: () => 'build',
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Layout from '../components/layout'
 import Head from 'next/head'
-
 export default class IndexPage extends React.Component {
   render() {
       return (
